9278e30e14c7abdb2e27a023f76967c522f84de8
alaingilbert <alain.gilbert.15@gmail.com>
2012-03-03 14:28:03 -0500

    A lot better blacklist example.

--- python_ttapi/examples/blacklist.py
+++ python_ttapi/examples/blacklist.py
@@ -9,17 +9,16 @@ USERID = 'xxxxxxxxxxxxxxxxxxxxxxxx'
 ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx'
 bot = Bot(AUTH, USERID, ROOMID)
 
-blackList = ['xxxxxxxxxxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxxxxxxxxxx']
+
+blackList = set(['xxxxxxxxxxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxxxxxxxxxx'])
 
 
 # Someone enter the room, make sure he's not on the blacklist.
 def registered(data):
    global blackList
    user = data['user'][0]
-   for userId in blackList:
-      if user['userid'] == userId:
-         bot.boot(userId, 'You are on the blacklist.')
-         break
+   if user['userid'] in blackList:
+      bot.boot(userId, 'You are on the blacklist.')
 
 
 bot.on('registered', registered)
