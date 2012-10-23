a88b259485bac8543aeaf530f9040d9bf93cb320
Mauvis Ledford <switchstatement@gmail.com>
2012-10-22 23:09:00 -0700

    Update turntable_data/pmmed.js
    Clarify with comments which user is sending and which is receiving.

--- turntable_data/pmmed.js
+++ turntable_data/pmmed.js
@@ -1,5 +1,5 @@
-{ senderid: '4deadb0f4fe7d013dc0555f1',
+{ senderid: '4deadb0f4fe7d013dc0555f1', // Person that sent PM.
   text: 'This is a test',
-  userid: '4e685aac14169c225510e99d',
+  userid: '4e685aac14169c225510e99d', // Person that received PM.
   command: 'pmmed',
   time: 1329875886.41 }
