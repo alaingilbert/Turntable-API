644ac73ff5eaaefca29657dca55acbbf8a421aca
alaingilbert <alain.gilbert.15@gmail.com>
2013-02-24 21:22:44 -0500

    playlist list all example data returned

---
+++ turntable_data/playlist_list_all.js
@@ -0,0 +1,16 @@
+{
+  "msgid": 6,
+  "list": [{
+    "active": true,
+    "name": "default"
+  },
+  {
+    "active": false,
+    "name": "test"
+  },
+  {
+    "active": false,
+    "name": "test2"
+  }],
+  "success": true
+}
