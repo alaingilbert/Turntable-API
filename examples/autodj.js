4cb67dce2d777ed65b79322ac7d9db87e557a332
Josh Zimmer <darthcoolness@gmail.com>
2013-04-24 17:37:28 -0400

    Fixed up Indentions in coding
    Fixed Indentions in coding.

--- examples/autodj.js
+++ examples/autodj.js
@@ -7,62 +7,62 @@ var getDownAfterSong = false;
 
 var bot = new Bot(AUTH, USERID, ROOMID);
 
- bot.on ('roomChanged',  function (data) {
- bot.roomInfo(true, function(data) {
+	bot.on ('roomChanged',  function (data) {
+	bot.roomInfo(true, function(data) {
  // Get the DJ count upon entering the room
- var djcount = data.room.metadata.djcount;
+	var djcount = data.room.metadata.djcount;
  // If DJ count less than or equal to 1, get on decks 	
-		if (djcount <= 1){
-		bot.addDj();
-		}
-		});
-		});
+if (djcount <= 1){
+        bot.addDj();
+	}
+	});
+	});
 
- bot.on ('newsong', function (data) {
+	bot.on ('newsong', function (data) {
  // Check if bot is the new DJ when new song begins
- var djid = data.room.metadata.current_song.djid;
-		if (djid == USERID){
-		imDjing = true;
-		}
-		});
+	var djid = data.room.metadata.current_song.djid;
+if (djid == USERID){
+        imDjing = true;
+	}
+	});
 
- bot.on ('endsong', function (data) {
+	bot.on ('endsong', function (data) {
  // Update 'imDjing' when Bot's song ends
- var djid = data.room.metadata.current_song.djid;
-		if (djid == USERID){
-		imDjing = false;
-		}
+	var djid = data.room.metadata.current_song.djid;
+if (djid == USERID){
+        imDjing = false;
+	}
 
  // If triggered to get down during Bot's song, step down now
-		if (getDownAfterSong == true){
-		bot.remDj(USERID);
-		getDownAfterSong = false;
-		}
-		});
+if (getDownAfterSong == true){
+        bot.remDj(USERID);
+        getDownAfterSong = false;
+	}
+	});
 
- bot.on('add_dj', function (data) {
+	bot.on('add_dj', function (data) {
  // Check the DJ count when a new DJ steps up
- bot.roomInfo (true, function(data) {
- var djcount = data.room.metadata.djcount;
+	bot.roomInfo (true, function(data) {
+	var djcount = data.room.metadata.djcount;
  // If there's enough DJ's now, bot steps down.	
-		if (djcount >= 3){
+if (djcount >= 3){
  // If bot's song is currently playing, let's have the bot step down when it ends
-		if (imDjing){
-		getDownAfterSong = true;
-		} else {
-		bot.remDj(USERID);
-		}
-		}
-		});
-		});
+if (imDjing){
+        getDownAfterSong = true;
+	} else {
+        bot.remDj(USERID);
+	}
+	}
+	});
+	});
 
- bot.on ('rem_dj', function (data) {
+	bot.on ('rem_dj', function (data) {
  // Checks DJ count when a DJ steps down
- bot.roomInfo (true, function(data) {
- var djcount = data.room.metadata.djcount;
+	bot.roomInfo (true, function(data) {
+	var djcount = data.room.metadata.djcount;
  // If there aren't enough DJ's, bot steps up
-		if (djcount <= 1){
-		bot.addDj();
-		}
-		});
-		});
+if (djcount <= 1){
+        bot.addDj();
+	}
+	});
+	});
