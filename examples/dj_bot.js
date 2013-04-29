8eaa19041f759b430f5304a52d30e7bf0b105198
Josh Zimmer <darthcoolness@gmail.com>
2013-04-29 10:56:42 -0400

    Changed Indentions
    Changed Indentions

--- examples/dj_bot.js
+++ examples/dj_bot.js
@@ -9,21 +9,21 @@ bot.on ('speak', function (data) {
   var text = data.text;
   if (text.match(/^\/go$/)) {
 // Bot gets on the DJ table (if there's a spot open) on /go command
-  bot.addDj();
+    bot.addDj();
   }
   if (text.match(/^\/stop$/)) {
 // Bot jumps off the table on /stop command
-  bot.remDj(USERID);
+    bot.remDj(USERID);
   }
   if (text.match(/^\/skip$/)) {
 // Bot skips it's own song (if bot is the current DJ) on /skip command
-  bot.skip();
+    bot.skip();
   }
   if (text.match(/^\/addsong$/)) {
 // Bot adds song to the bottom of it's DJ queue on /addsong command
-  bot.playlistAll(function (data) {
-  bot.playlistAdd(songId, data.list.length);
+    bot.playlistAll(function (data) {
+    bot.playlistAdd(songId, data.list.length);
   }); 
-  bot.snag();
+    bot.snag();
   }
   });
