c1f3ba3b25c0d34acf6b0c5a6cdc788239d3d5b4
alaingilbert <alain.gilbert.15@gmail.com>
2013-05-02 19:38:32 -0400

    reformat code

--- examples/dj_bot.js
+++ examples/dj_bot.js
@@ -7,19 +7,17 @@ var bot = new Bot(AUTH, USERID, ROOMID);
 
 bot.on('speak', function (data) {
   var text = data.text;
-  if (text.match(/^\/go$/)) {
+  if (text == '/go') {
     // Bot gets on the DJ table (if there's a spot open) on /go command
     bot.addDj();
-  }
-  if (text.match(/^\/stop$/)) {
+  } else if (text == '/stop') {
     // Bot jumps off the table on /stop command
-    bot.remDj(USERID);
-  }
-  if (text.match(/^\/skip$/)) {
+    bot.remDj();
+  } else if (text == '/skip') {
     // Bot skips it's own song (if bot is the current DJ) on /skip command
     bot.skip();
   }
-  if (text.match(/^\/addsong$/)) {
+  if (text == '/addsong') {
     // Bot adds song to the bottom of it's DJ queue on /addsong command
     bot.playlistAll(function (data) {
       bot.playlistAdd(songId, data.list.length);
