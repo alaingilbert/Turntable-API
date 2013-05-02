66c1418b582dff69d00aa9d602f23c83e869cfbe
Josh Zimmer <darthcoolness@gmail.com>
2013-05-01 20:50:57 -0400

    Fixed Coding 3.0
    Fixed Coding in AutoSnag, Auto DJ Bot, and Manual DJ Bot.

--- examples/autosnag.js
+++ examples/autosnag.js
@@ -6,7 +6,7 @@ var ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 var bot = new Bot(AUTH, USERID, ROOMID);
 
 bot.on('newsong', function (data) {
-    bot.snag();
-    bot.playlistAdd(data.room.metadata.current_song._id);
-    bot.becomeFan(data.room.metadata.current_dj);
+  bot.snag();
+  bot.playlistAdd(data.room.metadata.current_song._id);
+  bot.becomeFan(data.room.metadata.current_dj);
 });
