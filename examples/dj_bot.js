a74b608c276c63c5f3ebd8e4373e9e4af6c6b9bb
Marcello Africano <mnafricano@gmail.com>
2013-06-09 10:37:34 -0400

    Update dj_bot.js

--- examples/dj_bot.js
+++ examples/dj_bot.js
@@ -7,16 +7,16 @@ var bot = new Bot(AUTH, USERID, ROOMID);
 
 bot.on('speak', function (data) {
   var text = data.text;
-  if (text == '/go') {
+  if (text == '.get up') {
     // Bot gets on the DJ table (if there's a spot open) on /go command
     bot.addDj();
-  } else if (text == '/stop') {
+  } else if (text == '.get down') {
     // Bot jumps off the table on /stop command
     bot.remDj();
-  } else if (text == '/skip') {
+  } else if (text == '.skip') {
     // Bot skips it's own song (if bot is the current DJ) on /skip command
     bot.skip();
-  } else if (text == '/addsong') {
+  } else if (text == '.snag') {
     // Bot adds song to the bottom of it's DJ queue on /addsong command
     bot.playlistAll(function (data) {
       bot.playlistAdd(songId, data.list.length);
